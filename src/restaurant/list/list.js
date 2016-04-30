46cee6cb5a16ede20e152e26bbb5c46b3d3936c8
Wai-Yin Kwan <waiyin@gophilosophie.com>
2016-04-30 14:42:38 -0700

    implement view model

--- src/restaurant/list/list.js
+++ src/restaurant/list/list.js
@@ -3,17 +3,66 @@ import Map from 'can/map/';
 import 'can/map/define/';
 import './list.less!';
 import template from './list.stache!';
+
+// models
 import Restaurant from 'place-my-order/models/restaurant';
+import State from 'place-my-order/models/state';
+import City from 'place-my-order/models/city';
 
 export const ViewModel = Map.extend({
+  // define rules for each property
   define: {
+    // getList returns a promise.
+    states: {
+      get() {
+        return State.getList({});
+      }
+    },
+    // user selects state
+    state: {
+      type: 'string',
+      // default value is null
+      value: null,
+      set() {
+        // Remove the city when the state changes
+        this.attr('city', null);
+      }
+    },
+    cities: {
+      // when state property changes, we get new list of cities 
+      get() {
+        let state = this.attr('state');
+
+        if(!state) {
+          return null;
+        }
+
+        // if state exists, get a list of cities for the state
+        return City.getList({ state });
+      }
+    },
+    // user selects city
+    city: {
+      type: 'string',
+      value: null
+    },
+
     // add restaurants property that can be access in template.
     // restaurants will have a list a restaurants from the server
-
-    // restaurants returns a promise.
     restaurants: {
-      value() {
-        return Restaurant.getList({});
+      get() {
+        let state = this.attr('state');
+        let city = this.attr('city');
+
+        // if state and city is state, getList of restaurants
+        if(state && city) {
+          return Restaurant.getList({
+            'address.state': state,
+            'address.city': city
+          });
+        }
+
+        return null;
       }
     }
   }
